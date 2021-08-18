3aaae9f528fcccedd2dac5968fc7f1552d017295
epourgh <epourgh@gmail.com>
2021-08-17 23:34:43 -0500

    sass style globals

--- frontend/src/components/banner.component.tsx
+++ frontend/src/components/banner.component.tsx
@@ -5,7 +5,7 @@ import styles from '../styles/_banner.module.scss';
 const BannerComponent = () => (
   <div className={styles.bannerContent}>
     <h1 className={styles.titleStyle}>
-      <Link to="/"></Link>
+      <Link to="/">Home</Link>
     </h1>
   </div>
 );
--- frontend/src/pages/home.page.tsx
+++ frontend/src/pages/home.page.tsx
@@ -3,8 +3,10 @@ import styles from '../styles/_home.module.scss';
 
 const HomePage = () => {
   return (
-    <div>
-      Home
+    <div className={styles.wrapper}>
+      <div className={styles.content}>
+        <h1>Home</h1>
+      </div>
     </div>
   );
 };
